d496d94e1de3916ba9ca0466bbd21b38f3c271fc
Croissant, Adam <adam_croissant@homedepot.com>
2019-02-12 15:12:53 -0600

    connect cloud func to datastore

--- tracker-backend/games.go
+++ tracker-backend/games.go
@@ -1,30 +1,44 @@
 package p
 
 import (
+	"context"
 	"net/http"
 	"encoding/json"
 	"os"
+	"cloud.google.com/go/datastore"
+	"time"
 )
 
 type Game struct {
-	Date		string
+	Date		time.Time
 	Opponent	string
 	Result		string
 	DidAttend	bool
 }
 
 func GetGames(w http.ResponseWriter, r *http.Request) {
-	game := Game{
-		Date: "01/01/2019",
-		Opponent: "Express",
-		Result: "Win",
-		DidAttend: true,
-	}
-	result := []Game{game}
-	js, err := json.Marshal(result)
+	ctx := context.Background()
+	client, err := datastore.NewClient(ctx, "")
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-    		return
+		return
+	}
+
+    q := datastore.NewQuery("Game")
+
+	var games []Game
+    _, err = client.GetAll(ctx, q, &games)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+	js, err := json.Marshal(games)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+    	return
 	}
 	os.Stdout.Write(js)
 	w.Header().Set("Content-Type", "application/json")
--- tracker-backend/go.mod
+++ tracker-backend/go.mod
@@ -1,1 +1,3 @@
 module cloudfunction
+
+require cloud.google.com/go v0.36.0
